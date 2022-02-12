0f189c6c6c42055598a16342d54ef416e1407494
une pomme <AstafevK@yandex.ru>
2022-02-12 18:16:54 +0300

    LabWork6

---
+++ Task1/Abiturient.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Abiturient
+    {
+        private string fullName;
+        private int baseClass;
+        private double avg;
+
+        public string FullName1 { get => fullName; set => fullName = value; }
+
+        public Abiturient()
+            : this ("Иванов Иван Иванович", 11, 4.6)
+        {
+            
+        }
+
+        Abiturient abiturient1 = new Abiturient("Петров Петров Петров", 11, 5);
+        
+
+
+        public Abiturient(string fullName, int baseClass, double avg)
+        {
+            this.fullName = fullName;
+            this.baseClass = baseClass;
+            this.avg = avg;
+        }
+
+        Abiturient[] abiturient;
+        public object this [string index]
+        {
+            get => index.ToString();
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index == 1)
+                {
+                    return fullName;
+                }
+                if (index == 2)
+                {
+                    return baseClass.ToString();
+                }
+                if (index == 3)
+                {
+                    return avg.ToString();
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static double Square(int length)
+        {
+            return (length * 4);
+        }
+        static double Square(int length, int width)
+        {
+            return (length * 2) + (width * 2);
+        }
+
+        static double Degree(double x, int n)
+        {
+            if (n > 0)
+            {
+                double tempX = x;
+                for (int i = 1; i < n; i++)
+                {
+                    tempX *= x;
+                }
+                return tempX;
+            }
+            if (n == 0)
+                return 1;
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Квадрат по 1 стороне: length = 4");
+            Console.WriteLine(Square(4));
+            Console.WriteLine();
+            Console.WriteLine("Квадрат по 1 стороне: length = 4, width = 5");
+            Console.WriteLine(Square(4, 5));
+            Console.WriteLine();
+            Console.WriteLine("Степень. 2 в 5");
+            Console.WriteLine(Degree(2,5));
+            Console.WriteLine();
+            Console.WriteLine("Степень. 4 в 0");
+            Console.WriteLine(Degree(4, 0));
+            Console.WriteLine();
+            Console.WriteLine("Степень. 30 в -100");
+            Console.WriteLine(Degree(30, -100));
+        }
+
+        static void GAbiturient()
+        {
+            Abiturient abiturient = new Abiturient();
+            abiturient.FullName1 = "";
+        }
+
+        static void Abiturient(string FullName, int Klass, double AVG)
+        {
+
+        }
+    }
+}
